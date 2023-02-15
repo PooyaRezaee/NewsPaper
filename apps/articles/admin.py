0e46189d42322f53d7d5d3c19111b5793120400b
PooyaRezaee <pooya.rezaee.official@gmail.com>
2023-02-15 20:23:26 +0330

    Created Models Article,Category and Register It on admin Panel

--- apps/articles/admin.py
+++ apps/articles/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Article,Category
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name','count_article')
+    
+    def count_article(self,obj):
+        count = obj.article.count()
+        return str(count)
+    count_article.short_description = 'count'
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title','author','created')
+    list_filter = ('created','modified')
+
--- apps/articles/models.py
+++ apps/articles/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.urls import reverse
+from django.conf import settings
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=32)
+
+class Article(models.Model):
+    title = models.CharField(max_length=64)
+    body = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL,null=True,related_name='article')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL,null=True,related_name='article')
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("article_detail", kwargs={"pk": self.pk})
