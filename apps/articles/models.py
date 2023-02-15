3ca1b9f42d271ad8bb9f485e7f0b454f216fbe1c
PooyaRezaee <pooya.rezaee.official@gmail.com>
2023-02-15 21:56:11 +0330

    Add Operation Read Update Delete

--- apps/articles/models.py
+++ apps/articles/models.py
@@ -17,4 +17,4 @@ class Article(models.Model):
         return self.title
     
     def get_absolute_url(self):
-        return reverse("article_detail", kwargs={"pk": self.pk})
+        return reverse("article:article_detail", kwargs={"pk": self.pk})
--- apps/articles/views.py
+++ apps/articles/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView,DeleteView,UpdateView
 from .models import Article
+from django.urls import reverse_lazy
 
 __all__ = [
     'ArticleListView',
+    'ArticleDetailView',
+    'ArticleUpdateView',
+    'ArticleDeleteView',
 ]
 
 class ArticleListView(ListView):
     model = Article
     template_name = "article/article_list.html"
     context_object_name = 'articles'
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = "article/article_detail.html"
+    context_object_name = 'article'
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    fields = ("title","body")
+    template_name = "article/article_edit.html"
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = "article/article_delete.html"
+    success_url = reverse_lazy("article:article_list")
